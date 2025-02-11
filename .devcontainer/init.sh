f10ca62a1c9bcc1c2cecea9ed4ee5c8406e11b9f
Marco <Napster134@hotmail.com>
2025-02-11 23:47:05 +0000

    init commit with problematic code

---
+++ .devcontainer/init.sh
@@ -0,0 +1,6 @@
+sudo apt update
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+mkdir -p $DOCKER_CONFIG/cli-plugins
+curl -SL https://github.com/docker/compose/releases/download/v2.32.4/docker-compose-$(uname -s)-$(uname -m) -o $DOCKER_CONFIG/cli-plugins/docker-compose
+chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+sudo chmod 666 /var/run/docker.sock

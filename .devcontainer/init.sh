a87eed61961a0552c93a91fc39e226af190ca93b
Marco <Napster134@hotmail.com>
2025-02-12 03:34:51 +0000

    test

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,8 +1,7 @@
 sudo apt update
 
-# Install instructions from
+# docker compose install instructions from
 # https://docs.docker.com/compose/install/linux/#install-the-plugin-manually
-
 DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
 mkdir -p $DOCKER_CONFIG/cli-plugins
 curl -SL https://github.com/docker/compose/releases/download/v2.32.4/docker-compose-Linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Codespace-test
+
+# Bind mounts of existing image files fail when using docker outside of docker

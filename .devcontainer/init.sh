84526175200d846b0f5dec4232fd2d3708b39e41
Marco <Napster134@hotmail.com>
2025-02-11 19:09:43 -0800

    Update init.sh

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -4,3 +4,7 @@ mkdir -p $DOCKER_CONFIG/cli-plugins
 curl -SL https://github.com/docker/compose/releases/download/v2.32.4/docker-compose-$(uname -s)-$(uname -m) -o $DOCKER_CONFIG/cli-plugins/docker-compose
 chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
 sudo chmod 666 /var/run/docker.sock
+
+sleep 10
+
+docker compose up

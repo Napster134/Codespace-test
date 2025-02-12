f41ee8a6e34d51d547a1167623fb54bc31d8bb6c
Marco <Napster134@hotmail.com>
2025-02-12 03:15:02 +0000

    remove dockerfile ref

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,7 +1,11 @@
 sudo apt update
+
+# Install instructions from
+# https://docs.docker.com/compose/install/linux/#install-the-plugin-manually
+
 DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
 mkdir -p $DOCKER_CONFIG/cli-plugins
-curl -SL https://github.com/docker/compose/releases/download/v2.32.4/docker-compose-$(uname -s)-$(uname -m) -o $DOCKER_CONFIG/cli-plugins/docker-compose
+curl -SL https://github.com/docker/compose/releases/download/v2.32.4/docker-compose-Linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
 chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
 sudo chmod 666 /var/run/docker.sock
 

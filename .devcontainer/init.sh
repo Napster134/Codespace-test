0acc0df397aae541082bbde0457d610ca2220035
Marco <Napster134@hotmail.com>
2025-02-11 19:36:00 -0800

    Update init.sh

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,13 +1,1 @@
-sudo apt update
-
-# docker compose install instructions from
-# https://docs.docker.com/compose/install/linux/#install-the-plugin-manually
-DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
-mkdir -p $DOCKER_CONFIG/cli-plugins
-curl -SL https://github.com/docker/compose/releases/download/v2.32.4/docker-compose-Linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
-chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
-sudo chmod 666 /var/run/docker.sock
-
-sleep 10
-
 docker compose up
